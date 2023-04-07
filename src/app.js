f1d23226fe032d455ba641cee5d7e1df00b6af4b
Jorge Neto <jorge.c.neto@hotmail.com>
2023-04-06 23:08:03 -0300

    sign-up e tweets

--- src/app.js
+++ src/app.js
@@ -2,7 +2,43 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
+app.use(cors())
 
+const user = []
+const tweets = []
 
+app.post("/sign-up", (req, res) => {
+    const { username, avatar} = req.body
+
+    if(!username || !avatar){
+        return res.sendStatus(400)
+    }
+
+    if((typeof username !== "string") || (typeof avatar !== "string")){
+        return res.send("Todos os campos são obrigatórios!")
+    }
+
+    const newUser = {username, avatar}
+
+    user.push(newUser)
+    res.status(201).send("OK")
+})
+
+app.post("/tweets", (req, res) => {
+    if(user === ""){
+        return res.status(401).send("UNAUTHORIZED")
+    }
+
+    const { username, tweet } = req.body
+
+    if((typeof username !== "string") || (typeof tweet !== "string")){
+        return res.send("Todos os campos são obrigatórios!")
+    }
+    
+    const tweet = {username, tweet}
+
+    tweets.push(tweet)
+    res.status(201).send("OK")
+})
 
 app.listen(5000)
